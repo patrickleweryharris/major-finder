7e8cbe9f51310caf5cd8701b6820da1bed6b5a4b
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2016-05-18 00:56:31 -0400

    Working on importing JSON file
    I was using an external URL when I was trying to edit locally. Great
    waste of 20 minutes

--- major-finder.js
+++ major-finder.js
@@ -1,5 +1,13 @@
 // See README.md for algorithm design
-
+function load(){
+  var posts;
+  $.getJSON('http://raw.githubusercontent.com/patrickleweryharris/major-finder/master/majors.json', function(data){
+    console.log(data);
+    posts = data;
+  });
+  window.alert(posts[0].name);
+  console.log(posts[0].requirements[3]);
+}
 // Input santization ----------
 
 // ----------------------------
