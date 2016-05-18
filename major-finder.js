64e0b766dc50ac399fa27a85b1b896f3d8ca91c3
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2016-05-18 01:35:34 -0400

    Adding document header

--- major-finder.js
+++ major-finder.js
@@ -1,4 +1,20 @@
-// See README.md for algorithm design
+/*
+
+Major Finder 0.0.1
+
+Find majors you are eligible for at UofT
+
+Copyright (c) 2016 Patrick Harris (plh.io)
+
+Licensed under the MIT License
+https://raw.githubusercontent.com/patrickleweryharris/major-finder/master/LICENSE
+
+
+Project site: https://plh.io/major-finder
+Github site: http://github.com/patrickleweryharris/major-finder
+
+*/
+
 function load(){
   $.getJSON('http://raw.githubusercontent.com/patrickleweryharris/major-finder/master/majors.json', function(data){
     console.log("JSON loaded");
