1b0f3aed22b3e5df6b68761265aafa960c26e6ff
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-05-12 18:11:37 -0400

    found list of programs not included

---
+++ parse/parse.py
@@ -0,0 +1,38 @@
+import json
+from pprint import pprint
+
+'''
+ Find which program codes are not in the majors.json file
+'''
+
+
+def parse():
+    code_file = open("codes.txt", "r")
+    codes = []
+
+    for line in code_file:
+        codes.append(line)
+
+    with open('../json/majors.json') as data_file:
+        data = json.load(data_file)
+
+    included = set()
+    for program in data:
+        curr = program['calLink'][-4:]
+        included.add(curr)
+
+    not_included = []
+    for code in codes:
+        code = code[5:9]
+        if code not in included:
+            not_included.append(code)
+
+    
+    # print(not_included)
+    for item in not_included:
+        print(item)
+        # Prints nicer for me to read
+
+
+if __name__ == "__main__":
+    parse()
