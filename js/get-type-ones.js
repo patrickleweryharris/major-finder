33c85eba68742c8364326868969b0341a2cd4db5
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-06-02 19:39:35 -0400

    Extra type one results page

---
+++ js/get-type-ones.js
@@ -0,0 +1,31 @@
+var jsonStr;
+var seenPrograms = [];
+
+function getTypeOnes() {
+  jsonStr = 'https://raw.githubusercontent.com/patrickleweryharris/major-finder/master/json/majors.json';
+  $.getJSON(jsonStr, function(data) {
+    type1Output = "You are automatically eligible for the following <br> Type 1 Programs: <ul>";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].type == "1"){
+
+        if ($.inArray(data[i].postName, seenPrograms) == -1){
+
+          seenPrograms.push(data[i].postName);
+          type1Output = type1Output.concat("<li>");
+          type1Output = type1Output.concat("<a href=");
+          type1Output = type1Output.concat(data[i].calLink);
+          type1Output = type1Output.concat(">");
+          type1Output = type1Output.concat(data[i].postName);
+          type1Output = type1Output.concat("</a></li>");
+        }
+
+      }
+    }
+    type1Output.concat("</ul>");
+
+    document.getElementById("eligible_programs").innerHTML = type1Output;
+  });
+}
+
+window.onload = getTypeOnes;
